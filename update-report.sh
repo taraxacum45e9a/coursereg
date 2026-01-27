bd35bd45e3bb7767ffa53441785a2d903c80af4f
Shen Jiamin <shen_jiamin@comp.nus.edu.sg>
2026-01-27 20:39:37 +0800

    fix: update PDF URLs in update-report.sh to correct paths

--- update-report.sh
+++ update-report.sh
@@ -7,19 +7,19 @@ export TZ=Asia/Singapore
 set -ex
 
 cat <<EOF | wget --no-verbose -i -
-https://www.nus.edu.sg/ModReg/docs/DemandAllocationRptGD_R0.pdf
-https://www.nus.edu.sg/ModReg/docs/DemandAllocationRptGD_R1.pdf
-https://www.nus.edu.sg/ModReg/docs/DemandAllocationRptGD_R2.pdf
-https://www.nus.edu.sg/ModReg/docs/DemandAllocationRptGD_R3.pdf
-https://www.nus.edu.sg/ModReg/docs/DemandAllocationRptUG_R0.pdf
-https://www.nus.edu.sg/ModReg/docs/DemandAllocationRptUG_R1.pdf
-https://www.nus.edu.sg/ModReg/docs/DemandAllocationRptUG_R2.pdf
-https://www.nus.edu.sg/ModReg/docs/DemandAllocationRptUG_R3.pdf
-https://www.nus.edu.sg/ModReg/docs/VacancyRpt_AftR3.pdf
-https://www.nus.edu.sg/ModReg/docs/VacancyRpt_R0.pdf
-https://www.nus.edu.sg/ModReg/docs/VacancyRpt_R1.pdf
-https://www.nus.edu.sg/ModReg/docs/VacancyRpt_R2.pdf
-https://www.nus.edu.sg/ModReg/docs/VacancyRpt_R3.pdf
+https://nus.edu.sg/coursereg/docs/DemandAllocationRptGD_R0.pdf
+https://nus.edu.sg/coursereg/docs/DemandAllocationRptGD_R1.pdf
+https://nus.edu.sg/coursereg/docs/DemandAllocationRptGD_R2.pdf
+https://nus.edu.sg/coursereg/docs/DemandAllocationRptGD_R3.pdf
+https://nus.edu.sg/coursereg/docs/DemandAllocationRptUG_R0.pdf
+https://nus.edu.sg/coursereg/docs/DemandAllocationRptUG_R1.pdf
+https://nus.edu.sg/coursereg/docs/DemandAllocationRptUG_R2.pdf
+https://nus.edu.sg/coursereg/docs/DemandAllocationRptUG_R3.pdf
+https://nus.edu.sg/coursereg/docs/VacancyRpt_AftR3.pdf
+https://nus.edu.sg/coursereg/docs/VacancyRpt_R0.pdf
+https://nus.edu.sg/coursereg/docs/VacancyRpt_R1.pdf
+https://nus.edu.sg/coursereg/docs/VacancyRpt_R2.pdf
+https://nus.edu.sg/coursereg/docs/VacancyRpt_R3.pdf
 EOF
 
 for file in *.pdf; do
